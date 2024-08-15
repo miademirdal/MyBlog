5013f97f9fe20cf5ee89c664b1c6e7d9d2988c8a
miademirdal <miademirdal@gmail.com>
2024-08-15 15:26:10 -0400

    Add header components
    creating the access to my resume and header content

---
+++ blogwebsite/src/home/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="">Download Resume</a>
+        <a href="#contact">Let's Connect</a>
+    </div>
+  )
+}
+
+export default CTA
---
+++ blogwebsite/src/home/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Mia Demirdal</h1>
+                <h5 className="text-light">Aspiring full time SWE!!</h5>
+            </div>
+        </header>
+    )
+}
+
