d90f18f1ebcee16b366def25c9259bb7a8bdea36
miademirdal <miademirdal@gmail.com>
2024-10-22 15:46:37 -0400

    Add React icons

--- blogwebsite/src/home/header/Header.jsx
+++ blogwebsite/src/home/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/ME2.jpg'
+import ME from '../../assets/ME2.jpg';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
     return (
@@ -11,6 +12,7 @@ const Header = () => {
                 <h1>Mia Demirdal</h1>
                 <h5 className="text-light">Junior at Wentworth IT!</h5>
                 <CTA />
+                <HeaderSocials />
 
                 <div className="me">
                     <img src={ME} alt="Picture of me" />
--- blogwebsite/src/home/header/HeaderSocials.jsx
+++ blogwebsite/src/home/header/HeaderSocials.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { TiSocialLinkedinCircular } from "react-icons/ti";
+import { PiGithubLogoFill } from "react-icons/pi";
+
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/miademirdal/" target='_blank'></a>
-      <a href="https://github.com/miademirdal" target='_blank'></a>
+      <a href="https://www.linkedin.com/in/miademirdal/" target='_blank'><TiSocialLinkedinCircular /></a>
+      <a href="https://github.com/miademirdal" target='_blank'><PiGithubLogoFill /></a>
+        
     </div>
   )
 }
